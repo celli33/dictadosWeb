00b2ac46afcf9228aa37addc24398a3583611a0b
celli33 <misachiva_@hotmail.com>
2017-10-12 12:16:47 -0500

    mi primer commit

---
+++ application/views/encabezado.php
@@ -0,0 +1,13 @@
+    <div class="navegacion">
+        <nav>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Iniciar sesión</a>
+            </li>
+            <li><a href="#">Registrarse</a>
+            </li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>
+        </nav>
+      </div><!--navegacion-->
---
+++ application/views/main.php
@@ -0,0 +1,8 @@
+<body>
+  <div class="hero">
+    <h1>Dictados musicales</h1>
+  </div>
+  <div class="container">
+    
+  </div>
+</body>
