443c3b1659e061716da22fa6a0d3357db0b5b3e0
luismoyotl <32849087+luismoyotl@users.noreply.github.com>
2017-10-16 13:15:15 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function(){
+  $( document ).ready(function(){$(".button-collapse").sideNav();})
+  $(document).ready(function(){
+   $('.parallax').parallax();
+ });
+
+
+
+});
+})();
