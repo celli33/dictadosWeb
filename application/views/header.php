dd2f54757c25f33284a5c3b90c2728cf365064f0
celli33 <misachiva_@hotmail.com>
2017-10-31 02:24:48 -0600

    Add files via upload

--- application/views/header.php
+++ application/views/header.php
@@ -1,8 +1,11 @@
-<!DOCTYPE html>
+
+<?php header('Access-Control-Allow-Origin: *'); ?>
   <html lang="en">
   <head>
+
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+
     <title>Dictados Musicales</title>
 
     <!-- CSS  -->
@@ -10,11 +13,12 @@
 
     <link href="<?php echo base_url();?>css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="<?php echo base_url();?>css/gallery-materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link rel="stylesheet" href="<?php echo base_url();?>css/estilos.css" type="text/css" media="screen,projection"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>css/main.css" type="text/css" >
 
     <body>
       <!--Import jQuery before materialize.js-->
-      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="<?php echo base_url();?>js/jquery.js"></script>
       <script type="text/javascript" src="<?php echo base_url();?>js/materialize.min.js"></script>
       <script src="<?php echo base_url();?>js/main.js" type="text/javascript"></script>
+      <script src="<?php echo base_url();?>js/ion.sound.min.js" type="text/javascript"></script>
     </body>
--- application/views/main.php
+++ application/views/main.php
@@ -1,27 +1,60 @@
+<?php  header("Access-Control-Allow-Origin: http://localhost"); ?>
 <body>
 	<div class="parallax-container">
 		<div class="parallax"><img src="<?php echo base_url();?>/img/image1.jpg"></div>
 	</div>
-	<div class="section white">
+	<div class="section white" id="notas-sueltas">
 		<div class="row container">
 		  <h2 class="header">Dictados Notas Sueltas</h2>
 		  <p class="grey-text text-darken-3 lighten-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
-		<div class="contenedor">
-			<div class="notas-sueltas">
-					<div class="clave">
-						<img src="<?php echo base_url();?>img/gcleff2" alt="clave de sol">
-					</div>
+		<div class="contenedor notasS" id="notasS">
+			<div class="datos-dictado">
+				<span class="notas-compas"></span>
+				<span class="num-compas"></span>
+				<span class="aciertos"></span>
 			</div>
 			<div class="pentagrama">
-					<span class="line"></span>
-					<span class="line"></span>
-					<span class="line"></span>
-					<span class="line"></span>
-					<span class="line"></span>
-			</div>
+				<div class="clave">
+					<img src="<?php echo base_url();?>img/gcleff2" alt="clave de sol">
+				</div>
+				<div class="line-space F6">
+						<span class="line"><span class="nota notaF6"></span></span>
+				</div>
+				<div class="line-space E6">
+						<span class="space"><span class="nota notaE6"></span></span>
+				</div>
+				<div class="line-space D6">
+						<span class="line"><span class="nota notaD6"></span></span>
+				</div>
+				<div class="line-space C6">
+						<span class="space"><span class="nota notaC6"></span></span>
+				</div>
+				<div class="line-space B5">
+						<span class="line"><span class="nota notaB5"></span></span>
+				</div>
+				<div class="line-space A5">
+						<span class="space"><span class="nota notaA5"></span></span>
+				</div>
+				<div class="line-space G5">
+						<span class="line"><span class="nota notaG5"></span></span>
+				</div>
+				<div class="line-space F5">
+						<span class="space"><span class="nota notaF5"></span></span>
+				</div>
+				<div class="line-space E5">
+						<span class="line"><span class="nota notaE5"></span></span>
+				</div>
+				<div class="line-space D5">
+						<span class="space"><span class="nota notaD5"></span></span>
+				</div>
+			</div><!---pentagrama-->
+		</div><!--contenedor-->
+		<a href="#notasS" class="button botonNotasSueltas"> abrir dictado de Práctica</a>
+		<div class="Calificacion">
+
 		</div>
-	</div>
+	</div><!--section-->
 	<div class="parallax-container">
 		<div class="parallax"><img src="<?php echo base_url();?>/img/image2.jpg"></div>
 	</div>
