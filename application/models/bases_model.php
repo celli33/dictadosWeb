ac157c2a2a681be8560f7be3ffec4d96cf3df0e8
LeonardoRT96 <32780692+LeonardoRT96@users.noreply.github.com>
2017-11-15 23:50:52 -0600

    Add files via upload

--- application/models/bases_model.php
+++ application/models/bases_model.php
@@ -8,13 +8,42 @@ class bases_model extends CI_Model {
     }
 
     public function getQuery($cadena){
-    	return $query = $this->db->query($cadena);
+    	$query = $this->db->query($cadena);
+        if($query->num_rows()>0)
+            return $query;
+        else
+            return FALSE;
     }
 
     public function registrarse($data){
+        $cadena="select * from persona where correo='".$data['email']."' || usuario='".$data['usuario']."'";
+        if($this->getQuery($cadena)!=FALSE)
+           return FALSE;
     	$cadena="insert into persona(nombre,a_paterno,a_materno,usuario,correo,contraseña) values('".$data['nombre']."','".$data['paterno']."','".$data['materno']."','".$data['usuario']."','".$data['email']."','".$data['contraseña']."');";
     	//echo var_dump($cadena);
     	$query = $this->db->query($cadena);
+        return TRUE;
+    }
+
+    public function esMaestro($datasession){
+        $cadena="select * from maestro where idPersona='".$datasession['idPersona']."'";
+        $query = $this->db->query($cadena);
+        //echo var_dump($query);
+        if($query->num_rows()>0)
+            return $query;
+        else
+            return FALSE;
+    }
+
+    public function ingresaMaestro($data){
+        $cadena="select * from maestro where idPersona=".$data['maestro']."";
+        if($this->getQuery($cadena)!=FALSE)
+           return FALSE;
+
+        $cadena="insert into maestro(numGrupos,idPersona) values(0,".$data['maestro'].");";
+        //echo var_dump($cadena);
+        $query = $this->db->query($cadena);
+        return TRUE;
     }
 
     public function iniciarSesion($data){
