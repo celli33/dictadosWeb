27038e6dd293a5b9e53ad8192eb782f6818ff52e
LeonardoRT96 <32780692+LeonardoRT96@users.noreply.github.com>
2017-11-20 12:50:34 -0600

    Add files via upload

---
+++ application/views/mainIni.php
@@ -0,0 +1,35 @@
+<body class="body-ini">
+<div class="slider" >
+    <ul class="slides">
+      <li>
+        <img src="<?php echo base_url();?>/img/image2_b.jpg">
+        <div class="caption center-align">
+          <h3>Mejora tu oído!</h3>
+          <h5 class="light grey-text text-lighten-3">Dictados de Notas Sueltas, Rítmicos y Armónicos.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="<?php echo base_url();?>/img/image4_b.jpg">
+        <div class="caption left-align">
+          <h3>Dictados de práctica</h3>
+          <h5 class="light grey-text text-lighten-3">Donde quieras y Cuando quieras.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="<?php echo base_url();?>/img/image3_b.jpg">
+        <div class="caption right-align">
+          <h3>Ingresa y realiza exámenes de práctica</h3>
+          <h5 class="light grey-text text-lighten-3">Además accede a grupos a cargo de maestros calificados.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="<?php echo base_url();?>/img/image5_b.jpg">
+        <div class="caption center-align">
+          <h3>Si eres Maestro, hay un espacio para tí!</h3>
+          <h5 class="light grey-text text-lighten-3">Sube tus propios dictados.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <br>
+</body>
