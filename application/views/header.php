dce73984b5000159be161b30aca4b7e3d63e2771
luismoyotl <32849087+luismoyotl@users.noreply.github.com>
2017-11-29 23:01:04 -0600

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
 <?php header('Access-Control-Allow-Origin: *'); ?>
   <html lang="en">
   <head>
-
+ 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
 
